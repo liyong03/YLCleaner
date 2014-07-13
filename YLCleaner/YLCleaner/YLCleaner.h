cebf924198600e4cb8849585fdf10b4d369f452b
liyong03 <liyong03@gmail.com>
2014-07-13 19:58:28 +0800

    init project

---
+++ YLCleaner/YLCleaner/YLCleaner.h
@@ -0,0 +1,13 @@
+//
+//  YLCleaner.h
+//  YLCleaner
+//
+//  Created by Yong Li on 7/13/14.
+//  Copyright (c) 2014 Yong Li. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+@interface YLCleaner : NSObject
+
+@end
